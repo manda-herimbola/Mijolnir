bf285656a342686593bb0386e6ecf867cb8f8f01
manda-herimbola <=>
2022-11-14 19:27:00 +0300

    #6 Add data task in the database

--- src/component/Home/TaskList/TaskInput/TaskInput.tsx
+++ src/component/Home/TaskList/TaskInput/TaskInput.tsx
@@ -9,13 +9,12 @@ const TaskInput = ({ index, service, title, description }: any) => {
     const classes: any = useStyles()
     const [ {til,num}, setTil ]: any = useState({ til: {}, num: 0 })
     const [ errorTask, setErrorTask ]: any = useState('')
+    const SaveTask: any = useSaveTask( index, service, setTil, setErrorTask )
 
     const titleValue: Array<any> = [ title, til.title ]
     const descValue: Array<any> = [ description, til.description ]
     const titleDefaultVal: Array<string> = [ 'Title', 'undefined' ]
     const descDefaultVal: Array<string> = [ 'Description', 'undefined' ]
-
-    const SaveTask: any = useSaveTask( index, service, setTil, setErrorTask )
 
     return (
         <Grid component="form"
@@ -27,15 +26,15 @@ const TaskInput = ({ index, service, title, description }: any) => {
             <p color="red">{ errorTask }</p>
 
             { titleValue[num] !== titleDefaultVal[num] ? <p>{ titleValue[num] }</p> :
-                    <TextField variant="standard"
-                               name="title"
-                               placeholder="Title"/> }
+                <TextField variant="standard"
+                           name="title"
+                           placeholder="Title"/>}
 
             { descValue[num] !== descDefaultVal[num] ? <p>{ descValue[num] }</p> :
-                    <TextField variant="standard"
-                               name="description"
-                               placeholder="Description"
-                               multiline/> }
+                <TextField variant="standard"
+                           name="description"
+                           placeholder="Description"
+                           multiline/>}
 
             { titleValue[num] !== titleDefaultVal[num] &&
                 descValue[num] !== descDefaultVal[num] ? <></> :
